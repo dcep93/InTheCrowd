b4590ea00f6b5ee654064345d985ad13244eaaba
dcep93 <dan@quizlet.com>
2021-08-03 21:01:43 -0700

    install

--- .github/workflows/deploy_to_firebase.sh
+++ .github/workflows/deploy_to_firebase.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 cd inthecrowd/build
 export GOOGLE_APPLICATION_CREDENTIALS="gac.json"
 echo "$1" > "$GOOGLE_APPLICATION_CREDENTIALS"
+npm install -g firebase-tools
 gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
 firebase init
 firebase init hosting
