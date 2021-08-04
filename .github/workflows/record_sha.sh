0d7f5f06dabad825ab34edd7a0e987e4d3fc3350
dcep93 <dan@quizlet.com>
2021-08-04 16:39:49 -0700

    date sha

--- .github/workflows/record_sha.sh
+++ .github/workflows/record_sha.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 cd inthecrowd/src/inthecrowd
-printf "// eslint-disable-next-line import/no-anonymous-default-export\nexport default \"%s\";\n" "$(git rev-parse HEAD)" > recorded_sha.tsx
+printf "const recorded_sha = \"%s %s\";\nexport default recorded_sha;\n" "$(TZ='America/Los_Angeles' date)" "$(git rev-parse HEAD)" > recorded_sha.tsx
