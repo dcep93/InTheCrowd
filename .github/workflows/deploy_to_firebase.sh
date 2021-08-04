07b823071969212b166b078a7b0673c5cbfc01a1
dcep93 <dan@quizlet.com>
2021-08-03 20:58:28 -0700

    try

--- .github/workflows/deploy_to_firebase.sh
+++ .github/workflows/deploy_to_firebase.sh
@@ -7,4 +7,5 @@ export GOOGLE_APPLICATION_CREDENTIALS="gac.json"
 echo "$1" > "$GOOGLE_APPLICATION_CREDENTIALS"
 gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
 firebase init
+firebase init hosting
 firebase deploy
