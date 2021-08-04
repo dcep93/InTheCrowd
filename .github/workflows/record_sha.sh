e1731f46cefbbda46872f71d4ded14138e10da53
dcep93 <dan@quizlet.com>
2021-08-03 21:47:01 -0700

    Revert "readable"
    This reverts commit a0c96deb926e99689aaed8ad43a8ac5bc3d81a61.

--- .github/workflows/record_sha.sh
+++ .github/workflows/record_sha.sh
@@ -3,9 +3,4 @@
 set -euo pipefail
 
 cd inthecrowd/src/inthecrowd
-read -r -d '' code << 'EOF'
-// eslint-disable-next-line import/no-anonymous-default-export
-export default "%s";
-
-EOF
-printf "$code\n" "$(git rev-parse HEAD)" > recorded_sha.tsx
+printf "// eslint-disable-next-line import/no-anonymous-default-export\nexport default \"%s\";\n" "$(git rev-parse HEAD)" > recorded_sha.tsx
