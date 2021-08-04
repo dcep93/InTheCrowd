a0c96deb926e99689aaed8ad43a8ac5bc3d81a61
dcep93 <dan@quizlet.com>
2021-08-03 21:44:10 -0700

    readable

--- .github/workflows/record_sha.sh
+++ .github/workflows/record_sha.sh
@@ -3,4 +3,9 @@
 set -euo pipefail
 
 cd inthecrowd/src/inthecrowd
-printf "// eslint-disable-next-line import/no-anonymous-default-export\nexport default \"%s\";\n" "$(git rev-parse HEAD)" > recorded_sha.tsx
+read -r -d '' code << 'EOF'
+// eslint-disable-next-line import/no-anonymous-default-export
+export default "%s";
+
+EOF
+printf "$code\n" "$(git rev-parse HEAD)" > recorded_sha.tsx
