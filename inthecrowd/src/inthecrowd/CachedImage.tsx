8a6b47bf462b4f4ff3a2de148b845cce0ae9f289
dcep93 <dcep93@gmail.com>
2024-05-29 17:04:37 +0200

    x

--- inthecrowd/src/inthecrowd/CachedImage.tsx
+++ inthecrowd/src/inthecrowd/CachedImage.tsx
@@ -7,19 +7,31 @@ export default function CachedImage(props: {
   const CACHE_NAME = "CachedImage";
   const [data, updateData] = useState<string | undefined>(undefined);
   useEffect(() => {
+    console.log("effect", props.src);
     caches.open(CACHE_NAME).then((cache) =>
       cache
         .match(props.src)
         .then((cachedResponse) =>
           cachedResponse
             ? cachedResponse
-            : fetch(props.src).then((networkResponse) => {
-                cache.put(props.src, networkResponse.clone());
+            : fetch("https://proxy420.appspot.com", {
+                method: "POST",
+                body: JSON.stringify({
+                  maxAgeMs: 30 * 24 * 60 * 60 * 1000,
+                  url: props.src,
+                  options: { base64: true },
+                }),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }).then((networkResponse) => {
+                cache.put(props.src, networkResponse);
                 return networkResponse;
               })
         )
-        .then((response) => response.blob())
-        .then((blob) => URL.createObjectURL(blob))
+        .then((response) => response.clone())
+        .then((response) => response.text())
+        .then((data) => `data:image/jpeg;base64,${data}`)
         .then(updateData)
     );
   }, [props.src]);
